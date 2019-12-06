9416a7c964ad77f1919813d39d0d20fa2c795d99
vladimirKartyshov <vovan198080@inbox.ru>
2019-12-06 17:58:40 +0300

    The aplication has been finished

--- app/src/main/java/com/example/geoapp/MainActivity.java
+++ app/src/main/java/com/example/geoapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.geoapp;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -8,10 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.geoapp.models.User;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
@@ -50,11 +53,76 @@ public class MainActivity extends AppCompatActivity {
                 showRegisterWindow();
             }
         });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSignInWindow();
+            }
+        });
+    }
+
+    private void showSignInWindow(){
+
+        //отображаем реализацию всплывающего окна
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);//внутри этого же класса
+        // будет отображаться это всплывающее окно
+        dialog.setTitle("Войти");//устанавливаем заголовок для окна
+        dialog.setMessage("Введите все данные для входа");//подпись под заголовком
+
+        // получаем шаблон всплывающего окна
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View sign_in_window = inflater.inflate(R.layout. sign_in_window,null);
+        //и устанавливаем этот шаблон,как шаблон для всплывающего окна(dialog)
+        dialog.setView( sign_in_window);
+
+        //получаем все данные из текстоывых полей шаблона всплывающего окна
+        final MaterialEditText email =  sign_in_window.findViewById(R.id.emailField);
+        final MaterialEditText pass =  sign_in_window.findViewById(R.id.passField);
+
+        //создаем две кнопки - отмена всплы-го окна и регистрация в базе данных
+        dialog.setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.setPositiveButton("Войти", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (TextUtils.isEmpty(email.getText().toString())) {//если пользователь не введет почту
+                    //Snackbar позволяет выводить ошибки в всплывающих окнах
+                    Snackbar.make(root, "Введите вашу почту", Snackbar.LENGTH_SHORT).show();
+                    return;//чтобы выходили из этой фу-и и дальше код не обрабатывался если ошибка
+                }
+                if (pass.getText().toString().length() < 5)
+                    Snackbar.make(root, "Введите ваш пароль, который более 5 символов", Snackbar.LENGTH_SHORT).show();
+                return;
+            }
+
+        });
+
+        auth.signInWithEmailAndPassword(email.getText().toString(),pass.getText().toString())
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(MainActivity.this, MapActivity.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Snackbar.make(root,"Ошибка авторизации ." + e.getMessage(),Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.show();
+
     }
     private void showRegisterWindow() {
         //отображаем реализацию всплывающего окна
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);//внутри этого же класса
-                                                       // будет отображаться это всплывающее окно
+        // будет отображаться это всплывающее окно
         dialog.setTitle("Зарегистрироваться");//устанавливаем заголовок для окна
         dialog.setMessage("Введите все данные для регистрации");//подпись под заголовком
 
@@ -95,8 +163,8 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if (pass.getText().toString().length() < 5)
                     Snackbar.make(root,"Введите ваш пароль, который более 5 символов",Snackbar.LENGTH_SHORT).show();
-                    return;
-                }
+                return;
+            }
         });
         //Если не было ошибок, то дальше регистрация пользователя
         auth.createUserWithEmailAndPassword(email.getText().toString(),pass.getText().toString())
@@ -110,7 +178,7 @@ public class MainActivity extends AppCompatActivity {
                         user.setEmail(pass.getText().toString());
 
                         //передаем этот обЪект с установл-ми значе-ми в базу данных
-                        users.child(user.getEmail())//пользователя идентифицируем по ключу(email)
+                        users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())//по определенному id
                                 .setValue(user)//и добавляем в таблицу users
 
                                 //обработчик событий который сработает, когда поль=ль успешно добавится
@@ -119,9 +187,14 @@ public class MainActivity extends AppCompatActivity {
                                     public void onSuccess(Void aVoid) {
                                         Snackbar.make(root,"Пользователь добавлен", Snackbar.LENGTH_SHORT).show();
                                     }
-                                });
+                                }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(root,"Ошибка регистрации. " + e.getMessage(), Snackbar.LENGTH_LONG).show();
+                            }
+                        });
                     }
                 });
-                   dialog.show();
+        dialog.show();
     }
 }
